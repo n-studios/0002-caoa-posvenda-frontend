3896321b508ab172a4fc4b1e017eb69f0e606b53
Breno Monteiro <brenomont85@gmail.com>
2025-02-06 13:35:51 -0300

    feat: creates TextArea component

---
+++ src/components/TextArea.jsx
@@ -0,0 +1,13 @@
+const TextArea = () => {
+  return (
+    <div className="text-area">
+      <textarea
+      />
+      <button>
+        Send
+      </button>
+    </div>
+  );
+};
+
+export default TextArea;
