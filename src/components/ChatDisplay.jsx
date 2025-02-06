86254b2bd0e765dbbf9abdd9c6f52c926285f342
Breno Monteiro <brenomont85@gmail.com>
2025-02-06 13:25:37 -0300

    feat: creates ChatDisplay component

---
+++ src/components/ChatDisplay.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Message from "./Message";
+
+const ChatDisplay = () => {
+  return (
+    <div className="chat-display">
+    </div>
+  );
+};
+
+export default ChatDisplay;
