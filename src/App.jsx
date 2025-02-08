3659b03ae6e1c53d39161ff809696ae901b10fe1
Breno Monteiro <brenomont85@gmail.com>
2025-02-08 11:42:24 -0300

    refactor: restructure project for better modularity and organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Chat from "./pages/Chat";
-import Trainer from "./pages/Trainer";
-import Inventory from "./pages/Inventory";
+import Header from "./components/layout-components/Header";
+import Home from "./pages/Home/Home";
+import Chat from "./pages/Chat/Chat";
+import Trainer from "./pages/Trainer/Trainer";
+import Inventory from "./pages/Inventory/Inventory";
 
 const App = () => {
   return (
